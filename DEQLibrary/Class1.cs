7e79ec78868a70303e66d4c856e49fc419b023ae
Christina <k-dorohova@mail.ru>
2017-03-21 19:39:41 +0300

    Create frame DEQUE

--- DEQLibrary/Class1.cs
+++ DEQLibrary/Class1.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DEQLibrary
 {
-    public class Class1
+    //Двойной узел
+    public class DoublyNode<T>
     {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        //данные
+        public T Data { get; set; }
+        //предыдущий
+        public DoublyNode<T> Previous { get; set; }
+        //следующий
+        public DoublyNode<T> Next { get; set; }
+    }
+
+    public class Deque<T> : IEnumerable<T>  // двусвязный список
+    {
+        DoublyNode<T> head; // головной/первый элемент
+        DoublyNode<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
     }
 }
